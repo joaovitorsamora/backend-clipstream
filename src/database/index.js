2f5b0662476da8feda001c73d5aed73a7af05347
yoshi00 <jvsamora00@gmail.com>
2024-07-29 20:05:28 -0300

    Revert "feat: create initial backend structure"
    This reverts commit e7750a16b25a94fdb01266ffcc25a2ed189b555a.

--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-const dbConfig = require('../config/database')
-const Video = require('../models/Video')
-
-const connection = new Sequelize(dbConfig)
-
-Video.init(connection);
-
-module.exports = connection
