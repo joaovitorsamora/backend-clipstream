1cd9f76088d70a1931fe2a57405de62aca17b5f9
yoshi00 <jvsamora00@gmail.com>
2024-07-31 12:22:31 -0300

    updated environment variable

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./router');
 const bodyParser = require('body-parser');
-
+require('dotenv/config')
 require('./database')
+
+const port = process.env.PORT || 3333;
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,4 +14,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT);
+app.listen(port);
