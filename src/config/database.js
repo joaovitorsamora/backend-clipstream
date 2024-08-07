8a97b752e3fba2a9e37c1c12541003ac4b273bfe
yoshi00 <jvsamora00@gmail.com>
2024-08-07 13:08:18 -0300

    updated environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  url: process.env.DATABASE_URL,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_NAME,
   define: {
     timestamps: true,
     underscored: true,
--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ require('dotenv/config');
 require('pg');
 require('./database');
 
+const PORT = process.env.PORT;
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 
 module.exports = app;
