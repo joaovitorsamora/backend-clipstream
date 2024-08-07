b0873b6e2b130b050999931fe1c788778f17dea7
yoshi00 <jvsamora00@gmail.com>
2024-08-06 21:21:24 -0300

    updated environmental variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv/config')
 module.exports = {
   dialect: 'postgres',
-  host: 'ep-restless-sound-a5nv3ilp.us-east-2.aws.neon.tech',
+  host: 'localhost',
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: 'backendVideo',
