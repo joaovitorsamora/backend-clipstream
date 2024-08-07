162b818e8ac0d2b5bfb150e4e7bfc2e7dbf4150b
yoshi00 <jvsamora00@gmail.com>
2024-08-06 21:29:57 -0300

    updated for serverless environment

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./router');
 const bodyParser = require('body-parser');
-require('dotenv/config')
-require('./database')
-
-const port = process.env.PORT || 4000;
+require('dotenv/config');
+require('./database');
 
 const app = express();
-app.use(bodyParser.json());
 
-app.use(cors());  
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
